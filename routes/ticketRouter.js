808877f8396aa65bfa18f651b81d388616fc4581
Ruslan Balkovyy <ruslan835@revature.com>
2025-03-12 17:17:31 -0400

    add rolecheck for routhes in ticketRouter

--- routes/ticketRouter.js
+++ routes/ticketRouter.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { submitTicket, getPendingTickets, processTickets, viewTicketsAsEmployee } = require("../controllers/ticketController");
+const authorizeRole = require("../middleware/roleMiddleware");
 const router = express.Router();
 
 
-router.get("/pending", getPendingTickets);
+router.get("/pending", authorizeRole, getPendingTickets);
 
 router.get("/history", viewTicketsAsEmployee);
 
 router.post("/", submitTicket);
 
-router.post("/:ticketId", processTickets);
+router.post("/:ticketId", authorizeRole, processTickets);
 
 module.exports = router;
